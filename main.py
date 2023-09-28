8990de4a013066f55c084432f33a9971f16308f1
nosatoufer <gdufour05@gmail.com>
2023-09-28 09:04:24 +0200

    Add multiprocess

--- main.py
+++ main.py
@@ -1,4 +1,27 @@
 import bot
+import multiprocessing
+import socket
+
+# This is for render
+def run_socket_server():
+    mytcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    mytcp.bind(('0.0.0.0', 8080))
+    mytcp.listen(1)
+    while(1):
+        mytcp.listen(1)
+
+
+def run_discord_bot():
+    bot.run_discord_bot()
+
 
 if __name__ == '__main__':
-    bot.run_discord_bot()
+
+    socket_server_process = multiprocessing.Process(target=run_socket_server)
+    run_discord_bot = multiprocessing.Process(target=run_discord_bot)
+
+    socket_server_process.start()
+    run_discord_bot.start()
+
+    socket_server_process.join()
+    run_discord_bot.join()
